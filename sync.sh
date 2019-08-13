e08c54c41c5479e68d215540e19f7a1fb135171c
Bruno Schliersmair <bruno@cospired.com>
2019-08-13 15:29:40 +0200

    exclude workspace files on sync, do not sync over git repository

--- sync.sh
+++ sync.sh
@@ -5,11 +5,16 @@ if [ ! -d "$1" ]; then
   exit 1;
 fi
 
+if [ -d "${1}/.git" ]; then
+  echo "error: target directory seems to be a git repository. aborting...";
+  exit 1;
+fi
+
 INTERVAL=2
 
 echo "Syncing directory content every ${INTERVAL}s to $1"
 while [ true ]; do
   echo -n ".";
-  rsync -a --exclude 'node_modules' --exclude 'android/build' . $1
+  rsync -a --exclude '.git' --exclude '.gitignore' --exclude '.npmignore' --exclude '.watchmanconfig' --exclude 'node_modules' --exclude 'android/build' --exclude '.idea' --exclude 'android/.idea' . $1
   sleep ${INTERVAL};
 done;
